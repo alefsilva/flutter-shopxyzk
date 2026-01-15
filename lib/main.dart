901550dd876e29ee2347fa20ba7fba1dcdbbcbfd
Alef Silva <alefsilva.front.ux@gmail.com>
2026-01-15 02:47:24 -0300

    feat - home: refactoring home into new file and changed the status bar color

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:shopeazy/home_screen.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(statusBarColor: Colors.black45),
+  );
   runApp(const ShopEzy());
 }
 
@@ -9,59 +14,6 @@ class ShopEzy extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        // appBar: AppBar(title: const Text('ShopEzy s')),
-        // body: Center(child: Text("Body..")),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Stack(
-                clipBehavior: Clip.none,
-                children: [
-                  Container(
-                    width: 300.0,
-                    height: 150.0,
-                    decoration: BoxDecoration(
-                      color: Colors.orange.shade100.withAlpha(-4),
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(150.0),
-                        topRight: Radius.circular(150.0),
-                      ),
-                    ),
-                  ),
-                  Positioned.fill(
-                    bottom: -20,
-                    child: Align(
-                      alignment: Alignment.bottomCenter,
-                      child: Text(
-                        "ShopEazy",
-                        style: TextStyle(
-                          fontSize: 45,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black87,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 50.0),
-              TextButton(
-                onPressed: () {
-                  debugPrint("Button pressed");
-                },
-                child: Text(
-                  "Open Store",
-                  style: TextStyle(fontSize: 15, color: Colors.orange.shade300),
-                ),
-              ),
-            ],
-          ),
-        ),
-        backgroundColor: Colors.orange.shade50,
-      ),
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomeScreen());
   }
 }
