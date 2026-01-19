c6c953125f6f5f55b0a7e54e9bd126c8c1187e6c
Alef Silva <alefsilva.front.ux@gmail.com>
2026-01-19 08:13:23 -0300

    fix: resolving warnings

--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,11 @@ void main() {
   SystemChrome.setSystemUIOverlayStyle(
     const SystemUiOverlayStyle(statusBarColor: Colors.black45),
   );
-  runApp(const shopeazy());
+  runApp(const ShopEazy());
 }
 
-class shopeazy extends StatelessWidget {
-  const shopeazy({super.key});
+class ShopEazy extends StatelessWidget {
+  const ShopEazy({super.key});
 
   @override
   Widget build(BuildContext context) {
